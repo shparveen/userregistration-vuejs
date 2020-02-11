182624954b626c233f95ecf0e65f03abbc9e8e9b
Parveen Shaikh <shparmeen@gmail.com>
2020-02-11 18:55:55 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import VeeValidate from 'vee-validate';
+
+import { store } from './_store';
+import { router } from './_helpers';
+import App from './app/App';
+
+Vue.use(VeeValidate);
+
+// setup fake backend
+import { configureBackend } from './_helpers';
+configureBackend();
+
+new Vue({
+    el: '#app',
+    router,
+    store,
+    render: h => h(App)
+});
